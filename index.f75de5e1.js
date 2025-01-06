70253543151ba7beca767adb9f11fd086a6eef65
Ruslan Ihnatenko <ua.ignatenko@gmail.com>
2025-01-06 21:27:36 +0100

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,22 @@
+"use strict";
+const listWrapper = document.querySelector("ul");
+const employeesList = [
+    ...listWrapper.children
+];
+function sortList(list) {
+    return list.sort((first, second)=>salaryToNumber(second.dataset.salary) - salaryToNumber(first.dataset.salary));
+}
+function salaryToNumber(number = "") {
+    return +number.slice(1).split(",").join("");
+}
+function getEmployees(list) {
+    delete listWrapper.children;
+    for (const el of list){
+        const text = el.textContent;
+        el.setAttribute("data-name", text);
+        listWrapper.appendChild(el);
+    }
+}
+getEmployees(sortList(employeesList));
+
+//# sourceMappingURL=index.f75de5e1.js.map
