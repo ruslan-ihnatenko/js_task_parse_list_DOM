5c853019fbef31065430290118eee1ab942b97e0
Ruslan Ihnatenko <ua.ignatenko@gmail.com>
2025-01-06 21:11:34 +0100

    Task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+const listWrapper = document.querySelector('ul');
+const employeesList = [...listWrapper.children];
+
+function sortList(list) {
+  return list.sort(
+    (first, second) =>
+      salaryToNumber(second.dataset.salary) -
+      salaryToNumber(first.dataset.salary),
+  );
+}
+
+function salaryToNumber(number = '') {
+  return +number.slice(1).split(',').join('');
+}
+
+function getEmployees(list) {
+  for (const el of list) {
+    listWrapper.appendChild(el);
+  }
+}
+
+getEmployees(sortList(employeesList));
